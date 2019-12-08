ed94e7a26544ba53fa26b20cb4e74f0e14ccbd75
Nigibob <55683489+Nigibob@users.noreply.github.com>
2019-12-08 14:35:48 +0100

    Add files via upload

--- Lab3NodeJS/src/metrics.ts
+++ Lab3NodeJS/src/metrics.ts
@@ -1,5 +1,15 @@
-import LevelDB = require('./leveldb')
+import { LevelDB } from "./leveldb"
 import WriteStream from 'level-ws'
+
+export class Metric {
+    public timestamp: string
+    public value: number
+
+    constructor(ts: string, v: number) {
+      this.timestamp = ts
+      this.value = v
+    }
+}
 
 export class MetricsHandler {
   private db: any
@@ -7,9 +17,7 @@ export class MetricsHandler {
   constructor(dbPath: string) {
     this.db = LevelDB.open(dbPath)
   }
-}
 
-export class MetricsHandler {
     public save(key: number, metrics: Metric[], callback: (error: Error | null) => void) {
       const stream = WriteStream(this.db)
       stream.on('error', callback)
@@ -19,4 +27,23 @@ export class MetricsHandler {
       })
       stream.end()
     }
+    public get(key: string, callback: (err: Error | null, result?: Metric[]) => void) {
+   const stream = this.db.createReadStream()
+   var met: Metric[] = []
+
+   stream.on('error', callback)
+     .on('data', (data: any) => {
+       const [_, k, timestamp] = data.key.split(":")
+       const value = data.value
+       if (key != k) {
+         console.log(`LevelDB error: ${data} does not match key ${key}`)
+       } else {
+         met.push(new Metric(timestamp, value))
+       }
+     })
+     .on('end', (err: Error) => {
+       callback(null, met)
+     })
+ }
+    public save
 }
--- Lab3NodeJS/src/server.ts
+++ Lab3NodeJS/src/server.ts
@@ -1,28 +1,43 @@
-var bodyparser = require('body-parser');
-var express = require('express');
-import MetricsHandler = require('metrics');
-var app = express();
+import express = require('express')
+import { MetricsHandler } from './metrics'
+import path = require('path')
+import bodyparser = require('body-parser')
 
-app.set('port', 1111)
+const app = express()
+const port: string = process.env.PORT || '8080'
+app.use(express.static(path.join(__dirname, '/../public')))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded())
 
-MetricsHandler = new MetricsHandler('./db/metrics')
+app.set('views', __dirname + "/../views")
+app.set('view engine', 'ejs');
 
-app.post('/metrics/:id', (req, res) => {
-  dbMet.save(req.params.id, req.body, (err) => {
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.get('/hello/:name', (req: any, res: any) => {
+  res.render('hello.ejs', {name: req.params.name})
+})
+
+app.get('/metrics/:id', (req: any, res: any) => {
+  dbMet.get(req.params.id, (err: Error | null, result?: any) => {
+    if (err) throw err
+    res.json(result)
+  })
+})
+
+app.post('/metrics/:id', (req: any, res: any) => {
+  dbMet.save(req.params.id, req.body, (err: Error | null) => {
     if (err) throw err
     res.status(200).send()
   })
 })
 
-app.get('/', (req, res) => {res.send('Hello')})
-
-app.get('/metrics/:id', (req, res) => {
-
-
+app.listen(port, (err: Error) => {
+  if (err) throw err
+  console.log(`Server is running on http://localhost:${port}`)
 })
-app.listen(
-  app.get('port'),
-  () => console.log(`server listening on ${app.get('port')}`)
-)
